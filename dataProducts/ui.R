fe06073f9dc0b9aed8990c86203ea9526ad8cca6
junuylia <junuylia@hotmail.com>
2014-06-20 14:03:32 -0400

    Update ui.R

--- dataProducts/ui.R
+++ dataProducts/ui.R
@@ -4,34 +4,45 @@ library(shiny)
 
 
 shinyUI(pageWithSidebar(
-  headerPanel("Project for Developing Data Products!"),
+  headerPanel("Dynamic Histogram and Confidence Interval"),
   sidebarPanel(
-p("Please enter your desired sample size (this is a simple demo, so please limit your sample size between 50 and 1000."),
-numericInput('samplesize','Sample size [50-1000]',50,min=50, max = 1000, step=1),
-h6("By clicking this button, we generate a random sample and the histogram of this sample is displayed on the right."),
 
-p("Please choose the task you would like to implement from below"),
-checkboxGroupInput("task","Choose the task:",
-                 c("Confidence Interval"="ci", "Hypothesis Testing"="ht")),
-h6("How about the significance level you would like to use?"),
-numericInput('alpha','Significance Level (alpha) [0.001-0.2]',0.05,min=0.001, max = 0.2,step=0.001),
-p("Here we use 0.05 as default, but you may move the slide bar to your desired one. Please keep in mind that"),
-code(" alpha = 1- confidence level (C)"),
-p("For hypothesis testing, which value would you like to test?"),
-numericInput('mu0','Population mean to be tested',48,min=0, max = 100,step=1),
-submitButton("submit")
-)  ,
+sliderInput('samplesize','Sample size:',500,min=50, max = 1000, step=5),
+ br(), 
+selectInput('dist',"Distribution:",choices=c('Binomial','Normal','Poisson','Uniform')),
+conditionalPanel(
+    condition='input.dist=="Poisson"',
+    sliderInput('lambda','Population Mean:',50,min=0, max = 100, step=1)
+    ),      
+conditionalPanel(
+    condition='input.dist=="Uniform"',
+    sliderInput('lb','Lower Bound:',0,min=-10, max = 10, step=0.5),
+    sliderInput('rng','Range:', 1,min=0.1, max = 20  , step=0.1)
+    ),      
+conditionalPanel(
+    condition='input.dist=="Binomial"',
+    sliderInput('N','Number of Trials:',10,min=3, max = 100, step=1),
+    sliderInput('p','Probability of Success:',0.5,min=0.01, max = 1, step=0.01)
+    ),      
+conditionalPanel(
+    condition='input.dist=="Normal"',
+    sliderInput('mu','Population Mean:',50,min=0, max = 100, step=1) ,
+    sliderInput('sigma','Standard Deviation:',1,min=0.1, max = 20, step=0.1)   
+    ),      
+ br(), 
+sliderInput('alpha','Significance Level (alpha) [0.001-0.2]',0.05,min=0.001, max = 0.2,step=0.001),
+
+      br(), p("Please keep in mind that"),
+code(" alpha = (1- confidence level (C))/2")
+) ,
   mainPanel(
-    h3('Welcome to our Demo page!'),
-p("For a random sample of "),
-verbatimTextOutput("osamplesize"),
-p("the histogram is"),
+    h3('What is the confidence interval for the population mean?'),
 plotOutput("sampleHist"),
-verbatimTextOutput("otask"),
-p("Please find the"),verbatimTextOutput("oC"),
-p(" confidence interval from the plot."),
-p("At"),verbatimTextOutput("oalpha"),
-p("significance level, we get the conclusion that"),
-verbatimTextOutput("omu0")
-  )
-))
+
+verbatimTextOutput("ci")
+
+
+
+)
+
+      ))
